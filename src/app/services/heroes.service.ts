73f9d0f5e9ad681b3da71822de32ee7b84ec0c6f
Enured <cristofer.segundo@gmail.com>
2018-12-26 18:19:18 -0800

    added search engine, pipes and details when searching Heroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -57,11 +57,32 @@ export class HeroesService {
       ];
 
     constructor() {
-        console.log('servicio listo para usar!!');
     }
 
     getHeroes(){
       return this.heroes;
+    }
+
+    getHeroe( idx: string){
+      return this.heroes[idx];
+    }
+
+    buscarHeroes( termino:string ){
+      
+      let heroesArr:Heroe[] = [];
+      termino = termino.toLowerCase();
+
+      for( let heroe of this.heroes){
+
+        let nombre = heroe.nombre.toLowerCase();
+        if( nombre.indexOf( termino ) >= 0 ){
+          heroesArr.push ( heroe )
+        }
+      }
+
+      return heroesArr;
+
+
     }
 }
 
